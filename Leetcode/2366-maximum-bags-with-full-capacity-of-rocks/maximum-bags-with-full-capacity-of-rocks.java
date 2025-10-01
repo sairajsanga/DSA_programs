b9139680b67ad9a8d0ecf0fbaa905fb97a04559c
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-10-01 21:35:17 +0530

    Time: 74 ms (5.30%) | Memory: 55.1 MB (95.11%) - LeetSync

--- Leetcode/2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.java
+++ Leetcode/2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.java
@@ -1,30 +1,13 @@
 class Solution {
-    class Pair{
-        int max;
-        int rocks;
-        public Pair(int max,int rocks){
-            this.max=max;
-            this.rocks=rocks;
-        }
-    }
+    
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        // int totalmaxrocks=0;
-        // for(int num:capacity){
-        //    totalmaxrocks+=num;
-        // }
-        // if(additionalRocks>totalmaxrocks){
-        //     return capacity.length;
-        // }
-        List<Pair> list=new ArrayList<>();
+        PriorityQueue<Integer> q=new PriorityQueue<>();
         for(int i=0;i<capacity.length;i++){
-            list.add(new Pair(rocks[i],capacity[i]-rocks[i]));
+            q.add(capacity[i]-rocks[i]);
         }
-
-        Collections.sort(list,(a,b)->a.rocks-b.rocks);
-
         int count=0;
-        for(Pair p:list){
-            int reqrocks=p.rocks;
+        for(int i=0;i<capacity.length;i++){
+            int reqrocks=q.poll();
             if(additionalRocks>=reqrocks){
                 additionalRocks-=reqrocks;
                 count++;
