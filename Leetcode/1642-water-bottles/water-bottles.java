90b3739e6f724769d407be327f90e5281c41052b
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-10-01 13:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (20.50%) - LeetSync

--- Leetcode/1642-water-bottles/water-bottles.java
+++ Leetcode/1642-water-bottles/water-bottles.java
@@ -1,5 +1,12 @@
 class Solution {
-    public int numWaterBottles(int numBottles, int numExchange) {
-        return numBottles + (numBottles - 1) / (numExchange - 1);
+        public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        while (numBottles >= numExchange) {
+            int remainder = numBottles % numExchange;
+            numBottles /= numExchange;
+            ans += numBottles;
+            numBottles += remainder;
+        }
+        return ans;
     }
 }
