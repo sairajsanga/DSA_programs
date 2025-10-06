0087977265ab4e3e40888f1c29eccad67ba2c898
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-10-06 13:13:58 +0530

    Time: 2079 ms (100.00%) | Memory: 48.5 MB (100.00%) - LeetSync

---
+++ Leetcode/4019-remove-k-balanced-substrings/remove-k-balanced-substrings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeSubstring(String s, int k) {
+        StringBuilder ans = new StringBuilder(s);
+        StringBuilder patternBuilder = new StringBuilder();
+        for (int i = 0; i < k; i++) {
+            patternBuilder.append('(');
+        }
+        for (int i = 0; i < k; i++) {
+            patternBuilder.append(')');
+        }
+        String pattern = patternBuilder.toString();
+        if(pattern.length() == 0) return s;
+
+        int i = 0;
+        while(i<=ans.length()-2*k){
+            if(ans.substring(i,i+2*k).equals(pattern)){
+                ans.delete(i,i+2*k);
+                i = Math.max(0,i-2*k);
+            }
+            else i++;
+        }
+        return ans.toString();
+    }
+}
