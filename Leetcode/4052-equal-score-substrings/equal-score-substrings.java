a597e9afc273d0b314c0405e3c557d633c3f2660
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-10-12 00:48:52 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (100.00%) - LeetSync

--- Leetcode/4052-equal-score-substrings/equal-score-substrings.java
+++ Leetcode/4052-equal-score-substrings/equal-score-substrings.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean scoreBalance(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<26;i++){
-            char ch=(char)('a'+i);
-           map.put(ch,i+1);
-        }
+        
         int total=0;
         for(int i=0;i<s.length();i++){
-            total+=map.get(s.charAt(i));
+            total+=s.charAt(i) - 'a' + 1;
         }
         int left=0;
         for(int i=0;i<s.length();i++){
-            left+=map.get(s.charAt(i));
+            left+=s.charAt(i) - 'a' + 1;
             if(total-left==left){
                 return true;
             }
