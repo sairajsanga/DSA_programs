f3d3b6d93ae7df83f08f9913c34a5f8e3a2637cc
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-10-16 11:25:42 +0530

    Time: 2 ms (39.27%) | Memory: 44.3 MB (80.56%) - LeetSync

--- Leetcode/3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
+++ Leetcode/3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
@@ -1,21 +1,26 @@
 class Solution {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
        int n=nums.size();
-
-       for(int i=0;i+2*k<=n;i++){
-           if(getValid(nums,i,i+k)&&getValid(nums,i+k,i+2*k)){
-              return true;
-           }  
-       }
+       int prev=0;
+       int  current=1;
+       int min=0;
+       for(int i=1;i<n;i++){
+            if(nums.get(i)>nums.get(i-1)){
+                current++;
+            }
+            else{
+                prev=current;
+                current=1;
+            }
+            if(current/2>=k){
+                return true;
+            }
+            
+            if(Math.min(prev,current)>=k){
+                return true;
+            }
+       } 
        return false;
-    }
-    public boolean getValid(List<Integer> nums, int i, int j) {
-    // If subarray length is 1, it's trivially increasing
-    if (j - i <= 1) return true;
 
-    for (int x = i; x < j - 1; x++) {  // stop at j-1 to avoid out-of-bounds
-        if (nums.get(x) >= nums.get(x + 1)) return false;
     }
-    return true;
-}
 }
