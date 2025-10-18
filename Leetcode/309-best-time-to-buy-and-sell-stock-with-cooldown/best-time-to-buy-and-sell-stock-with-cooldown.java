b24cf29de70940cbb0ad88973266195721306bd1
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-10-19 00:47:46 +0530

    Time: 28 ms (5.07%) | Memory: 45.2 MB (5.15%) - LeetSync

--- Leetcode/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ Leetcode/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,15 +1,16 @@
 class Solution {
-    int dp[][]=new int[5001][2];
+    
     public int maxProfit(int[] prices) {
         int n=prices.length;
         if(n<=1) return 0;
+        int dp[][]=new int[5001][2];
         for(int arr[]:dp){
             Arrays.fill(arr,-1);
         }
         boolean buy=true;
-        return solve(prices,0,n,buy);
+        return solve(prices,0,n,buy,dp);
     }
-    public int solve(int prices[],int index,int n,boolean buy){
+    public int solve(int prices[],int index,int n,boolean buy,int dp[][]){
         if(index>=prices.length){
             return 0;
         }
@@ -18,13 +19,13 @@ class Solution {
         }
         int profit=0;
         if(buy){
-            int take=solve(prices,index+1,n,false)-prices[index];
-            int notake=solve(prices,index+1,n,true);
+            int take=solve(prices,index+1,n,false,dp)-prices[index];
+            int notake=solve(prices,index+1,n,true,dp);
             profit=Math.max(take,notake);
         }
         else{
-            int sell=prices[index]+solve(prices,index+2,n,true);
-            int nosell=solve(prices,index+1,n,false);
+            int sell=prices[index]+solve(prices,index+2,n,true,dp);
+            int nosell=solve(prices,index+1,n,false,dp);
              profit=Math.max(sell,nosell);
         }
         return dp[index][buy?1:0]=profit;
