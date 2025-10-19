3105c76d9bea0b7d69ac1a54281d1813ce950674
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-10-19 22:37:44 +0530

    Time: 72 ms (49.44%) | Memory: 51.3 MB (22.47%) - LeetSync

--- Leetcode/1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
+++ Leetcode/1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
@@ -1,31 +1,37 @@
 class Solution {
+    String min;
+    HashSet<String> visited;
     public String findLexSmallestString(String s, int a, int b) {
-        Set<String> vis = new HashSet<>();
-        String smallest = s;
-        Deque<String> q = new ArrayDeque<>();
-        q.offer(s);
-        vis.add(s);
-
-        while (!q.isEmpty()) {
-            String cur = q.poll();
-            if (cur.compareTo(smallest) < 0)
-                smallest = cur;
-
-            char arr[] = cur.toCharArray();
-            int n = arr.length;
-            for (int i = 1; i < n; i += 2) {
-                int num = (arr[i] - '0');
-                num = (num + a) % 10;
-                arr[i] = (char) (num + '0');
-            }
-            String added = new String(arr);
-            if (vis.add(added))
-                q.offer(added);
-
-            String rotated = cur.substring(cur.length() - b) + cur.substring(0, cur.length() - b);
-            if (vis.add(rotated))
-                q.offer(rotated);
+        int n=s.length();
+        this.min = "9".repeat(n);
+        this.visited=new HashSet<>();
+        solve(s,a,b);
+        return min;
+    }
+    public void solve(String s,int a,int b){
+        if(s.compareTo(min)<0){
+            min=s;
         }
-        return smallest;
+        if(visited.contains(s)) return;
+
+        visited.add(s);
+
+        String added=addition(s,a);
+        String shift=shifted(s,b);
+        solve(added,a,b);
+        solve(shift,a,b);
+    }
+    public String addition(String s,int a){
+        char arr[]=s.toCharArray();
+        for(int i=1;i<arr.length;i+=2){
+            int num=arr[i]-'0';
+            num=(num+a)%10;
+            arr[i]=(char)(num+'0');
+        }
+        return new String(arr);
+    }
+    public String shifted(String s,int b){
+        String shift=s.substring(s.length()-b)+s.substring(0,s.length()-b);
+        return shift;
     }
 }
