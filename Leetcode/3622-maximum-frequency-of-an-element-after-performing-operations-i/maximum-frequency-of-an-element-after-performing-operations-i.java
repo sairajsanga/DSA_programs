e688f48416bd95158f9066e4026f8702a6709a1d
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-10-22 21:07:18 +0530

    Time: 11 ms (82.78%) | Memory: 56.9 MB (83.44%) - LeetSync

--- Leetcode/3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
+++ Leetcode/3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -1,23 +1,41 @@
 class Solution {
     public int maxFrequency(int[] nums, int k, int numOps) {
+        int n=nums.length;
         int maxVal = Arrays.stream(nums).max().getAsInt() + k + 2;
         int[] count = new int[maxVal];
+        int original[]=new int[maxVal];
+        
+        for (int v : nums){
+            original[v]++;
+        }
+        if(numOps==0){
+           int fastmax = Arrays.stream(original).max().getAsInt();
+           return fastmax;
+        }
+        for(int i=0;i<nums.length;i++){
+            int ele=nums[i];
+            int start=ele-k;
+            int end=ele+k;
+            if (start < 0) start = 0;
+               count[start]+=1;
 
-        for (int v : nums)
-            count[v]++;
-
-        for (int i = 1; i < maxVal; i++)
-            count[i] += count[i - 1];
-
-        int res = 0;
-        for (int i = 0; i < maxVal; i++) {
-            int left = Math.max(0, i - k);
-            int right = Math.min(maxVal - 1, i + k);
-            int total = count[right] - (left > 0 ? count[left - 1] : 0);
-            int freq = count[i] - (i > 0 ? count[i - 1] : 0);
-            res = Math.max(res, freq + Math.min(numOps, total - freq));
+            if(end+1<maxVal){
+                count[end+1]-=1;
+            }
         }
 
-        return res;
+        for(int i=1;i<maxVal;i++){
+            count[i]+=count[i-1];
+        }
+        int max=0;
+        for(int i=0;i<count.length;i++){
+            if(count[i]>0){
+                int operationsneeded=Math.min(count[i]-original[i],numOps);
+                if(operationsneeded<=numOps){
+                   max=Math.max(max,original[i]+operationsneeded);
+                }
+            }
+        }
+        return max;
     }
 }
