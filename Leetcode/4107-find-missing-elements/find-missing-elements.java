81eddeb29bb25f325e56d11298490824e39fb04a
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-11-02 13:19:02 +0530

    Time: 7 ms (0.00%) | Memory: 47.2 MB (0.00%) - LeetSync

--- Leetcode/4107-find-missing-elements/find-missing-elements.java
+++ Leetcode/4107-find-missing-elements/find-missing-elements.java
@@ -9,7 +9,7 @@ class Solution {
         for(int i=0;i<n;i++){
           set.add(nums[i]);
         }
-        for(int i=start;i<=end;i++){
+        for(int i=start+1;i<end;i++){
             if(!set.contains(i)){
                 list.add(i);
             }
