a70c50989da9b30dae87c024ac604f99c02315e4
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-11-15 00:16:25 +0530

    Time: 19 ms (63.16%) | Memory: 76.8 MB (23.68%) - LeetSync

--- Leetcode/2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ Leetcode/2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -1,27 +1,26 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
         
-        int result[][]=new int[n+1][n+1];
+        int result[][]=new int[n][n];
         for(int []query:queries){
             int row1=query[0];
             int col1=query[1];
             int row2=query[2];
             int col2=query[3];
 
-            result[row1][col1]+=1;
-            result[row2+1][col1]-=1;
-            result[row1][col2+1]-=1;
-            result[row2+1][col2+1]+=1;
-        }
-        int mat[][]=new int [n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int x1 = (i == 0) ? 0 : mat[i - 1][j];
-                int x2 = (j == 0) ? 0 : mat[i][j - 1];
-                int x3 = (i == 0 || j == 0) ? 0 : mat[i - 1][j - 1];
-                mat[i][j] = result[i][j] + x1 + x2 - x3;
+            for(int x=row1;x<=row2;x++){
+                result[x][col1]+=1;
+                if(col2+1<n){
+                    result[x][col2+1]-=1; 
+                }
             }
         }
-        return mat;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                result[i][j]+=result[i][j-1];
+            }
+        }
+        
+        return result;
     }
 }
