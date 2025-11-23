6a66d6c23b15daa370e37fef0ae69dbf6dc46027
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-11-23 19:39:12 +0530

    Time: 1 ms (99.96%) | Memory: 42.5 MB (86.02%) - LeetSync

--- Leetcode/4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ Leetcode/4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -10,14 +10,19 @@ class Solution {
           }
           n=n/10;
         }
-        long num=reverse(sum);
-        return num*total;
+        
+        long fix=0;
+        while(sum>0){
+            fix=fix*10+(sum%10);
+            sum=sum/10;
+        }
+        return fix*total;
     }
-    public long reverse(long num){
-        String s=String.valueOf(num);
-        StringBuilder str=new StringBuilder(s);
-        String res=str.reverse().toString();
-        Long ans=Long.parseLong(res);
-        return ans;
-    }
+    // public long reverse(long num){
+    //     String s=String.valueOf(num);
+    //     StringBuilder str=new StringBuilder(s);
+    //     String res=str.reverse().toString();
+    //     Long ans=Long.parseLong(res);
+    //     return ans;
+    // }
 }
