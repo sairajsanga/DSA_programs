17a896c35f940cbd9b72566432f24e1e96f0005f
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-12-10 13:41:57 +0530

    Time: 1 ms (100.00%) | Memory: 91.7 MB (44.00%) - LeetSync

--- Leetcode/3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
+++ Leetcode/3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
@@ -2,20 +2,12 @@ class Solution {
     int MOD=(int)1e9+7;
     public int countPermutations(int[] nums) {
         int n=nums.length;
-        boolean flag=false;
-       
         int ele=nums[0];
-        HashSet<Integer> set=new HashSet<>();
-        set.add(ele);
         for(int i=1;i<n;i++){
-            if(set.contains(nums[i])||nums[i]<ele){
-                flag=true;
-                break;
-            }
+            if(nums[i]<=ele) return 0;
         }
 
-        if(!flag) return factorial(n-1)%MOD;
-        return 0;
+        return factorial(n-1);
         
     }
    public int factorial(int n) {
