7866a2b509bb91043c078c156d0d6d0f58311d42
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-12-14 02:21:52 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (5.53%) - LeetSync

--- Leetcode/623-add-one-row-to-tree/add-one-row-to-tree.java
+++ Leetcode/623-add-one-row-to-tree/add-one-row-to-tree.java
@@ -15,54 +15,33 @@
  */
 class Solution {
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
-
-        if (root == null)
-            return null;
-
-        if (depth == 1) {
-            TreeNode newroot = new TreeNode(val);
-            newroot.left = root;
-
+        if(root==null) return null;
+        if(depth==1){
+            TreeNode newroot=new TreeNode(val);
+            newroot.left=root;
             return newroot;
         }
 
-        Queue<TreeNode> q = new LinkedList<>();
+        return dfs(root,val,1,depth);
+    }
+    public TreeNode dfs(TreeNode root,int val,int currdepth,int depth){
+        if (root == null)
+            return null;
+        if(currdepth==depth-1){
+            TreeNode lefttemp=root.left;
+            TreeNode righttemp=root.right;
 
-        q.offer(root);
-        int d = 1;
-        while (!q.isEmpty()) {
-            int size = q.size();
+            root.left=new TreeNode(val);
+            root.right=new TreeNode(val);
 
-            if (d == depth - 1) {
-                for (int i = 0; i < q.size(); i++) {
+            root.left.left=lefttemp;
+            root.right.right=righttemp;
+            return root;
+        }  
 
-                    TreeNode fakeroot = q.poll();
-                    TreeNode leftTemp = fakeroot.left;
-                    TreeNode rightTemp = fakeroot.right;
-
-                    fakeroot.left = new TreeNode(val);
-                    fakeroot.right = new TreeNode(val);
-
-                    fakeroot.left.left = leftTemp;
-                    fakeroot.right.right = rightTemp;
-
-                }
-            }
-            else{
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-
-                if (node.left != null) {
-                    q.offer(node.left);
-                }
-                if (node.right != null) {
-                    q.offer(node.right);
-                }
-            }
-            d++;
-            }
-
-        }
-        return root;
+        root.left=dfs(root.left,val,currdepth+1,depth);
+        root.right=dfs(root.right,val,currdepth+1,depth);
+        return root;  
+       
     }
 }
