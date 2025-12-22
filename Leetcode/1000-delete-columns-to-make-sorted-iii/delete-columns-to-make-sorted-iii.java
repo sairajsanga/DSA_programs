e5d99353d9b08a3c83adff44a23b559d3808660c
sairaj <154606239+sairajsanga@users.noreply.github.com>
2025-12-22 18:22:53 +0530

    Time: 8 ms (58.97%) | Memory: 44.3 MB (47.44%) - LeetSync

---
+++ Leetcode/1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minDeletionSize(String[] A) {
+        int m = A.length, n = A[0].length(), res = n - 1, k;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < j; ++i) {
+                for (k = 0; k < m; ++k) {
+                    if (A[k].charAt(i) > A[k].charAt(j))
+                        break;
+                }
+                if (k == m && dp[i] + 1 > dp[j])
+                    dp[j] = dp[i] + 1;
+            }
+            res = Math.min(res, n - dp[j]);
+        }
+        return res;
+    }
+}
