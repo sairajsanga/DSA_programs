164f90a5c94ea0303945f00ad37cea714f7668e6
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-04-19 16:00:57 +0530

    Time: 31 ms (6.44%) | Memory: 90.1 MB (39.32%) - LeetSync

--- Leetcode/1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
+++ Leetcode/1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
@@ -2,11 +2,30 @@ class Solution {
     public int maxDistance(int[] nums1, int[] nums2) {
         int n=nums1.length;
         int m=nums2.length;
-        int i,j;
-        for(i=0,j=0;i<n&&j<m;j++){
-            if(nums1[i]>nums2[j]) i++;
+       int max=0;
+       for(int i=0;i<n;i++){
+          int ele=nums1[i];
+          int idx=binarySearch(ele,0,m-1,nums2);
+          if(idx<0) continue;
+          max=Math.max(max,idx-i);
         }
+        return max;
+       
+    }
 
-        return Math.max(0,j-i-1);
+    public int binarySearch(int ele,int low,int high,int nums[]){
+       
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]>=ele){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low-1;
     }
 }
