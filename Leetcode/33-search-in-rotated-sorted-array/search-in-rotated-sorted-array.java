1597d7d53f138b11eacef983ec6d053e84b3eaf6
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-05-22 12:14:15 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (26.61%) - LeetSync

---
+++ Leetcode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,56 @@
+class Solution {
+
+    public int search(int[] arr, int target) {
+        int pivot=peakelement(arr);
+        
+        if(pivot==-1){
+           return binarysearch(arr,target,0,arr.length-1);
+        }
+        if(arr[pivot]==target){
+            return pivot;
+        }
+        
+        if(target>=arr[0]){
+            return binarysearch(arr,target,0,pivot-1);
+        }
+        
+        return binarysearch(arr,target,pivot+1,arr.length-1);
+    }
+
+    int peakelement(int []arr){
+        int start=0;
+        int end=arr.length-1;
+         while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(start<mid && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[start]>= arr[mid]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+         }
+         return -1;
+    }
+    int binarysearch(int arr[],int target,int start,int end){
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target>arr[mid]){
+                start=mid+1;
+            }
+            else if(target<arr[mid]){
+                end=mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
