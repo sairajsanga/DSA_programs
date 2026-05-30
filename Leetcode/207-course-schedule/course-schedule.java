ae6d15c3a531632bdc9ec700b5c9333496502aaf
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-05-30 15:22:22 +0530

    Time: 7 ms (58.94%) | Memory: 47 MB (75.93%) - LeetSync

--- Leetcode/207-course-schedule/course-schedule.java
+++ Leetcode/207-course-schedule/course-schedule.java
@@ -1,35 +1,32 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] nums) {
-        boolean visited[]=new boolean[numCourses];
-        boolean inRecursion[]=new boolean[numCourses];
-
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj=new ArrayList<>();
+        int indegree[]=new int[numCourses];
+        for(int i=0;i<numCourses;i++) adj.add(new ArrayList<>());
+        for(int nums[]:prerequisites){
+            adj.get(nums[1]).add(nums[0]);
+            indegree[nums[0]]++;
+        }
 
+        Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<numCourses;i++){
-            adj.add(new ArrayList<>());
+            if(indegree[i]==0){
+                q.add(i);
+            }
         }
 
-        for(int num[]:nums){
-            adj.get(num[1]).add(num[0]);
+        int count=0;
+        while(!q.isEmpty()){
+            int ele=q.poll();
+            count++;
+
+            for(int v:adj.get(ele)){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.add(v);
+                }
+            }
         }
-
-        for(int i=0;i<numCourses;i++){
-            if(!visited[i]&&DFS(adj,i,visited,inRecursion)) return false;
-        }
-        return true;
-    }
-
-    public boolean DFS( List<List<Integer>> adj,int u,boolean visited[],boolean inRecursion[]){
-
-        visited[u]=true;
-        inRecursion[u]=true;
-
-        for(int v:adj.get(u)){
-            if(!visited[v]&&DFS(adj,v,visited,inRecursion)) return true;
-            else if(inRecursion[v]) return true;
-        }  
-
-        inRecursion[u]=false;
-        return false;
+        return count==numCourses;
     }
 }
