5dff28006ee60d252ae0b645506033a8e8d038f7
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-05-31 19:46:01 +0530

    Time: 37 ms (5.31%) | Memory: 114.1 MB (5.77%) - LeetSync

--- Leetcode/2245-destroying-asteroids/destroying-asteroids.java
+++ Leetcode/2245-destroying-asteroids/destroying-asteroids.java
@@ -4,28 +4,30 @@ class Solution {
         int n=nums.length;
         Arrays.sort(nums);
 
-        int i=0;
-        long dup=mass;
-        while(i<n){
-            if(nums[i]>dup) return false;
-            else{
-                dup+=nums[i];    
-            }
-            i++;
-        }
+        // int i=0;
+        // long dup=mass;
+        // while(i<n){
+        //     if(nums[i]>dup) return false;
+        //     else{
+        //         dup+=nums[i];    
+        //     }
+        //     i++;
+        // }
 
-        return true;
+        // return true;
+
+        return DFS((long)mass,nums,0);
 
         
     }
 
-    // public boolean DFS(int mass,int asteroids[],int start){
-    //     if(mass<asteroids[start]) return false;
-    //     if(start>=asteroids.length) return false;
+    public boolean DFS(long mass,int asteroids[],int start){
+        if(mass<asteroids[start]) return false;
+        if(start>=asteroids.length) return false;
 
-    //     if(start==asteroids.length-1) return true;
+        if(start==asteroids.length-1) return true;
 
-    //     return DFS(mass+asteroids[start],asteroids,start+1);
-    // }
+        return DFS(mass+asteroids[start],asteroids,start+1);
+    }
 
 }
