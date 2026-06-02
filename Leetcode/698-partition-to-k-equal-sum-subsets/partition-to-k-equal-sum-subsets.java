a3b1a8569b5042a1e564699266302ae3704d2379
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-06-02 15:13:22 +0530

    Time: 4 ms (71.28%) | Memory: 42.9 MB (80.85%) - LeetSync

---
+++ Leetcode/698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,65 @@
+class Solution {
+
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+
+        int sum = 0;
+
+        for (int num : nums)
+            sum += num;
+
+        if (sum % k != 0)
+            return false;
+
+        int target = sum / k;
+
+        Arrays.sort(nums);
+
+        if (nums[nums.length - 1] > target)
+            return false;
+
+        boolean[] visited = new boolean[nums.length];
+
+        return dfs(nums, visited, k, 0, 0, target);
+    }
+
+    private boolean dfs(int[] nums,
+            boolean[] visited,
+            int k,
+            int start,
+            int currSum,
+            int target) {
+
+        if (k == 1)
+            return true;
+
+        if (currSum == target)
+            return dfs(nums, visited, k - 1, 0, 0, target);
+
+        for (int i = start; i < nums.length; i++) {
+
+            if (visited[i])
+                continue;
+
+            if (currSum + nums[i] > target)
+                continue;
+
+            visited[i] = true;
+
+            if (dfs(nums,
+                    visited,
+                    k,
+                    i + 1,
+                    currSum + nums[i],
+                    target))
+                return true;
+
+            visited[i] = false;
+
+            if(currSum==0) break;
+        }
+
+        
+
+        return false;
+    }
+}
