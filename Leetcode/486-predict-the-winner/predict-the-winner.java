c269416f1db9540cfcfb7ea45222aefc98b27619
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-08-04 14:11:06 +0530

    Time: 72 ms (7.94%) | Memory: 42.8 MB (35.25%) - LeetSync

--- Leetcode/486-predict-the-winner/predict-the-winner.java
+++ Leetcode/486-predict-the-winner/predict-the-winner.java
@@ -1,35 +1,18 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;
-        int sum = 0;
-        boolean turn = true;
+        int n=nums.length;
 
-        for (int num : nums) {
-            sum += num;
-        }
-
-        int playerOneMax = getPlayer1Max(0, start, end, nums, turn);
-
-        if (sum % 2 == 1) {
-            return playerOneMax >= (sum + 1) / 2;
-        } else {
-            return playerOneMax >= sum / 2;
-        }
+        return solve(nums,0,n-1)>=0;
     }
 
-    private int getPlayer1Max(int player1, int start, int end, int[] nums, boolean turn) {
-        if (start > end)
-            return player1;
+    public int solve(int nums[],int i, int j){
+        if(i>j) return 0;
 
-        if (turn) {
-            int left = getPlayer1Max(player1 + nums[start], start + 1, end, nums, false);
-            int right = getPlayer1Max(player1 + nums[end], start, end - 1, nums, false);
-            return Math.max(left, right);
-        } else {
-            int left = getPlayer1Max(player1, start + 1, end, nums, true);
-            int right = getPlayer1Max(player1, start, end - 1, nums, true);
-            return Math.min(left, right);
-        }
+        if(i==j) return nums[i]; 
+
+        int left=nums[i]-solve(nums,i+1,j);
+        int right=nums[j]-solve(nums,i,j-1);
+
+        return Math.max(left,right);
     }
 }
