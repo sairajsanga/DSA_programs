849c680ecfbf53199ca855c56a035817a342d5ef
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-08-25 11:39:39 +0530

    Time: 2 ms (68.20%) | Memory: 45.5 MB (26.41%) - LeetSync

--- Leetcode/4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ Leetcode/4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,19 +1,17 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
+        int n=nums.length;
+
         HashSet<Integer> set=new HashSet<>();
-        int max=0;
+        
         for(int ele:nums){
             set.add(ele);
-            if(ele>max) max=ele;
         }
 
-        int n=nums.length;
-        for(int i=1;i<=max+1;i++){
-            int multiple=k*i;
-            if(!set.contains(multiple)){
-                return multiple;
-            }
+        for(int i=1;i<=101;i++){
+            if(!set.contains(k*i)) return k*i;
         }
+
         return -1;
     }
 }
