10244eabfafbfda6cb5feb9194ed913f64fd8d9d
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-08-29 23:18:36 +0530

    Time: 132 ms (28.79%) | Memory: 239.6 MB (9.85%) - LeetSync

--- Leetcode/3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
+++ Leetcode/3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,41 +1,128 @@
-class Solution {
-    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
-        int n=nums.length;
-        
-        int newnums[]=new int[n];
-        for (int i = 0; i < nums.length; i++) newnums[i] = nums[i];
+import java.util.*;
 
+public class Solution {
 
-        Arrays.sort(newnums);
+    static class Pair {
+        int value;
+        int index;
 
-        HashMap<Integer,Integer> numToGroup=new HashMap<>();
-        int grpnum=0;
-        numToGroup.put(newnums[0],grpnum);
+        Pair(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+    }
 
-        HashMap<Integer,LinkedList<Integer>> grpToList=new HashMap<>();
+    static int[] parent;
+    static int[] rank;
 
-        grpToList.put(grpnum,new LinkedList<Integer>(Arrays.asList(newnums[0])));
+    // Find with path compression
+    static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
 
-        for(int i=1;i<n;i++){
-            if(Math.abs(newnums[i]-newnums[i-1])>limit){
-                grpnum++;
-            }
+    // Union by rank
+    static void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
 
-            numToGroup.put(newnums[i],grpnum);
-            if(!grpToList.containsKey(grpnum)){
-                grpToList.put(grpnum,new LinkedList<Integer>());
-            }
-            grpToList.get(grpnum).add(newnums[i]);
+        if (pa == pb) {
+            return;
         }
 
-        int result[]=new int [n];
-        for(int i=0;i<n;i++){
-            int num=nums[i];
-            int group=numToGroup.get(num);
+        if (rank[pa] < rank[pb]) {
+            parent[pa] = pb;
+        } 
+        else if (rank[pa] > rank[pb]) {
+            parent[pb] = pa;
+        } 
+        else {
+            parent[pb] = pa;
+            rank[pa]++;
+        }
+    }
 
-            nums[i]=grpToList.get(group).pollFirst();
+    public static int[] lexicographicallySmallestArray(int[] nums, int limit) {
+
+        int n = nums.length;
+
+        parent = new int[n];
+        rank = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
 
-        return nums;
+        // Store value + original index
+        Pair[] arr = new Pair[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Pair(nums[i], i);
+        }
+
+        // Sort according to value
+        Arrays.sort(arr, (a, b) -> Integer.compare(a.value, b.value));
+
+        /*
+         * If two adjacent values in sorted order differ
+         * by <= limit, they belong to the same component.
+         */
+        for (int i = 1; i < n; i++) {
+
+            if ((long) arr[i].value - arr[i - 1].value <= limit) {
+                union(arr[i].index, arr[i - 1].index);
+            }
+        }
+
+        /*
+         * Group indices and values according to their
+         * connected component.
+         */
+        Map<Integer, List<Integer>> indices = new HashMap<>();
+        Map<Integer, List<Integer>> values = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+
+            int root = find(i);
+
+            indices.computeIfAbsent(root, k -> new ArrayList<>()).add(i);
+            values.computeIfAbsent(root, k -> new ArrayList<>()).add(nums[i]);
+        }
+
+        /*
+         * For every component:
+         *
+         * smallest index  <- smallest value
+         * second index     <- second smallest value
+         * ...
+         */
+        int[] result = new int[n];
+
+        for (int root : indices.keySet()) {
+
+            List<Integer> idxList = indices.get(root);
+            List<Integer> valueList = values.get(root);
+
+            Collections.sort(idxList);
+            Collections.sort(valueList);
+
+            for (int i = 0; i < idxList.size(); i++) {
+                result[idxList.get(i)] = valueList.get(i);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 5, 3, 9, 8};
+        int limit = 2;
+
+        int[] result = lexicographicallySmallestArray(nums, limit);
+
+        System.out.println(Arrays.toString(result));
     }
 }
