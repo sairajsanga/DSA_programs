923820ad0882281d84a88043599feab00dac5e0e
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-08-31 19:26:07 +0530

    Time: 133 ms (11.45%) | Memory: 101.8 MB (23.49%) - LeetSync

--- Leetcode/1788-stone-game-vi/stone-game-vi.java
+++ Leetcode/1788-stone-game-vi/stone-game-vi.java
@@ -1,58 +1,46 @@
 class Solution {
-    class Pair{
-        int value;
-        int idx;
-        public Pair(int idx,int value){
-     
-            this.idx=idx;
-            this.value=value;
-        }
-    }
+
     public int stoneGameVI(int[] aliceValues, int[] bobValues) {
 
-        int n=aliceValues.length;
+        int n = aliceValues.length;
 
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> b[1] - a[1]);
 
-        PriorityQueue<Pair> q=new PriorityQueue<>((a,b)->b.value-a.value);
-
-
-        for(int i=0;i<n;i++){
-            q.offer(new Pair(i,aliceValues[i]+bobValues[i]));
+        for (int i = 0; i < n; i++) {
+            q.offer(new int[] { i, aliceValues[i] + bobValues[i] });
         }
 
-        int aliceScore=0;
-        int bobScore=0;
-        int person=1;
-        boolean visited[]=new boolean[n];
+        int aliceScore = 0;
+        int bobScore = 0;
+        int person = 1;
+        boolean visited[] = new boolean[n];
 
-        while(!q.isEmpty()){
-            Pair curr=q.poll();
-            int val=curr.value;
-            int index=curr.idx;
+        while (!q.isEmpty()) {
+            int curr[] = q.poll();
+            int val = curr[1];
+            int index = curr[0];
 
-             if(person==1){
-             
-                if(visited[index]) continue;
+            if (person == 1) {
+                if (visited[index])
+                    continue;
+                aliceScore += aliceValues[index];
+                visited[index] = true;
+                person = 0;
 
-                aliceScore+=aliceValues[index];
-                visited[index]=true;
-                person=0;
-
-             }
-             else{
-    
-                if(visited[index]) continue;
-
-                bobScore+=bobValues[index];
-                visited[index]=true;
-                person=1;
-             }
+            } else {
+                if (visited[index])
+                    continue;
+                bobScore += bobValues[index];
+                visited[index] = true;
+                person = 1;
+            }
         }
-        
-        System.out.println(aliceScore);
-        System.out.println(bobScore);
-        if(aliceScore>bobScore) return 1;
-        else if(aliceScore<bobScore) return -1;
-        else return 0;
+
+        if (aliceScore > bobScore)
+            return 1;
+        else if (aliceScore < bobScore)
+            return -1;
+        else
+            return 0;
     }
 }
