8d01de174879626d9947c2ef7b89d322c4153359
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-08-31 22:47:27 +0530

    Time: 20 ms (16.77%) | Memory: 138 MB (18.06%) - LeetSync

--- Leetcode/3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
+++ Leetcode/3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -1,22 +1,29 @@
 class Solution {
+    int dp[];
+
     public int maximumEnergy(int[] energy, int k) {
         int n=energy.length;
-       int suffix[]=new int[energy.length];
-       
-       for(int i=n-1;i>=0;i--){
-          if(i+k>=n){
-            suffix[i]=energy[i];
-          }
-          else{
-            suffix[i]=energy[i]+suffix[i+k];
-          }
-       }
-       int max=Integer.MIN_VALUE;
-       for(int i=0;i<suffix.length;i++){
-           if(max<suffix[i]){
-            max=suffix[i];
-           }
-       }
-       return max;
+        int max=Integer.MIN_VALUE;
+
+        this.dp=new int[n+1];
+        Arrays.fill(dp,Integer.MIN_VALUE);
+
+        for(int i=0;i<n;i++){
+           max=Math.max(max,solve(energy,i,k));
+        }
+        return max;
+    }
+
+    public int solve(int nums[],int idx,int k){
+
+        if(idx>=nums.length) return 0;
+
+        if(dp[idx]!=Integer.MIN_VALUE) return dp[idx];
+
+
+        int take=nums[idx]+solve(nums,idx+k,k);
+    
+        
+        return dp[idx]=take;
     }
 }
