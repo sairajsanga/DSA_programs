07303f912b63444ab1cdf3f000ee71d9372ac650
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-08-31 15:27:09 +0530

    Time: 6 ms (21.25%) | Memory: 51.5 MB (85.51%) - LeetSync

--- Leetcode/200-number-of-islands/number-of-islands.java
+++ Leetcode/200-number-of-islands/number-of-islands.java
@@ -1,36 +1,58 @@
 class Solution {
     int n;
     int m;
+    int directions[][];
+    boolean visited[][];
     public int numIslands(char[][] grid) {
+
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
         this.n=grid.length;
         this.m=grid[0].length;
-        
+
+        this.visited=new boolean[n][m];
+        this.directions=new int[][]{
+            {0,1},
+            {0,-1},
+            {1,0},
+            {-1,0}
+        };
         if (n == 0) return 0;
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                     dfs(grid,i,j);
+                if(grid[i][j]=='1'&&!visited[i][j]){
+                     bfs(grid,i,j);
                      count++;
                 }
             }
         }
-
-
         return count;
     }
 
 
-    public void dfs(char grid[][],int i,int j){
+    public void bfs(char grid[][],int i,int j){
+        
 
-        if(i>=n||j>=m||i<0||j<0||grid[i][j]=='0') return;
+        Queue<int[]> q=new LinkedList<>();
+        q.add(new int[]{i,j});
 
-        grid[i][j]='0';
+        visited[i][j]=true;
 
-        dfs(grid,i,j+1);
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j-1);
+        while(!q.isEmpty()){
+            int curr[]=q.poll();
+            
+            for(int dir[]:directions){
+                int r=curr[0]+dir[0];
+                int c=curr[1]+dir[1];
 
+
+                if(r>=0 && r<n && c>=0 && c<m && grid[r][c]=='1' && !visited[r][c]){
+                    visited[r][c]=true;
+                    q.add(new int[]{r,c});
+                }
+            }
+        }
     }
 }
