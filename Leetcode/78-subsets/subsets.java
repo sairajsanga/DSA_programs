1737ce1831ea131141de76260c9176210b336c8d
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-09-01 00:03:22 +0530

    Time: 1 ms (86.57%) | Memory: 44 MB (88.21%) - LeetSync

--- Leetcode/78-subsets/subsets.java
+++ Leetcode/78-subsets/subsets.java
@@ -1,25 +1,22 @@
 class Solution {
+    List<List<Integer>> result;
     public List<List<Integer>> subsets(int[] nums) {
         int n=nums.length;
-
-        List<List<Integer>> result=new ArrayList<>();
-        List<Integer> innerList=new ArrayList<>();
-
-        helper(nums,0,result,innerList);
+        this.result=new ArrayList<>();
+        solve(nums,0,new ArrayList<>());
         return result;
     }
-
-    public void helper(int nums[],int start,List<List<Integer>> result,List<Integer> list){
-
-        if(start==nums.length){
+    public void solve(int nums[],int idx,List<Integer> list){
+        if(idx>nums.length) return;
+        if(idx==nums.length){
             result.add(new ArrayList<>(list));
             return;
         }
 
-
-        list.add(nums[start]);
-        helper(nums,start+1,result,list);
-        list.remove(list.size()-1);
-        helper(nums,start+1,result,list);        
+        int num=nums[idx];
+        list.add(num);
+        solve(nums,idx+1,list);
+        list.removeLast();
+        solve(nums,idx+1,list);
     }
 }
