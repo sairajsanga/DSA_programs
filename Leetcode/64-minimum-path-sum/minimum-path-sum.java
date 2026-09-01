c6f88d6f5211386adbd20f934db20e92d5806c29
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-09-01 20:20:45 +0530

    Time: 1 ms (99.83%) | Memory: 50.2 MB (56.60%) - LeetSync

--- Leetcode/64-minimum-path-sum/minimum-path-sum.java
+++ Leetcode/64-minimum-path-sum/minimum-path-sum.java
@@ -1,51 +1,27 @@
 class Solution {
-    int dist[][];
-    int directions[][];
     int m;
     int n;
+    int dp[][];
     public int minPathSum(int[][] grid) {
         this.m=grid.length;
         this.n=grid[0].length;
-        this.directions=new int[][]{
-            {0,1},
-            {1,0}
-        };
-
-        this.dist=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dist[i],Integer.MAX_VALUE);
+        this.dp=new int[m+1][n+1];
+        for(int d[]:dp){
+            Arrays.fill(d,Integer.MAX_VALUE);
         }
-        solve(grid,0,0);
-
-        return dist[m-1][n-1]==Integer.MAX_VALUE?-1:dist[m-1][n-1]+grid[0][0];
+        return solve(grid,0,0);
     }
 
-    public void solve(int grid[][],int startR,int startC){
+    public int solve(int grid[][],int startR,int startC){
+        if(startR>=m||startC>=n) return Integer.MAX_VALUE;
 
-        PriorityQueue<int[]> q=new PriorityQueue<>((a,b)->a[2]-b[2]);
-        dist[startR][startC]=0;
-        q.add(new int[]{startR,startC,0});
-        while(!q.isEmpty()){
-            int curr[]=q.poll();
-            int row=curr[0];
-            int col=curr[1];
-            int cost=curr[2];
+        if(startR==m-1&&startC==n-1) return grid[startR][startC];
 
-            if(cost>dist[row][col]) continue;
+        if(dp[startR][startC]!=Integer.MAX_VALUE) return dp[startR][startC];
+        
+        int right=solve(grid,startR,startC+1);
+        int down=solve(grid,startR+1,startC);
 
-            for(int dir[]:directions){
-                int newR=row+dir[0];
-                int newC=col+dir[1];
-
-                if(newR<0||newC<0||newR>=m||newC>=n) continue;
-
-                int newCost=cost+grid[newR][newC];
-
-                if(newCost<dist[newR][newC]){
-                    dist[newR][newC]=newCost;
-                    q.add(new int[]{newR,newC,newCost});
-                }
-            }
-        } 
+        return dp[startR][startC]=grid[startR][startC]+Math.min(right,down);
     }
 }
