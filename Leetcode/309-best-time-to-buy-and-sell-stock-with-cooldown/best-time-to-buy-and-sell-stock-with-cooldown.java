0b633b4dbee2a2968cecac2621a843455df08b12
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-09-03 21:19:08 +0530

    Time: 1 ms (69.06%) | Memory: 43.8 MB (10.19%) - LeetSync

--- Leetcode/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ Leetcode/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,33 +1,31 @@
 class Solution {
-    
-    public int maxProfit(int[] prices) {
-        int n=prices.length;
-        if(n<=1) return 0;
-        int dp[][]=new int[5001][2];
-        for(int arr[]:dp){
-            Arrays.fill(arr,-1);
-        }
-        boolean buy=true;
-        return solve(prices,0,n,buy,dp);
+    Integer dp[][];
+    public int maxProfit(int[] nums) {
+        int n=nums.length;
+        this.dp=new Integer[nums.length+1][2];
+        
+        return solve(nums,0,0);
     }
-    public int solve(int prices[],int index,int n,boolean buy,int dp[][]){
-        if(index>=prices.length){
-            return 0;
-        }
-        if(dp[index][buy?1:0]!=-1){
-            return dp[index][buy?1:0];
-        }
-        int profit=0;
-        if(buy){
-            int take=solve(prices,index+1,n,false,dp)-prices[index];
-            int notake=solve(prices,index+1,n,true,dp);
-            profit=Math.max(take,notake);
+
+    public int solve(int nums[],int idx,int state){
+
+        if(idx>=nums.length) return 0;
+        if(dp[idx][state]!=null) return dp[idx][state];
+
+        int stock=nums[idx];
+        int maxProfit=0;
+
+        if(state==0){
+            int buy=solve(nums,idx+1,1)-stock;
+            int skip=solve(nums,idx+1,0);
+            maxProfit=Math.max(buy,skip);
         }
         else{
-            int sell=prices[index]+solve(prices,index+2,n,true,dp);
-            int nosell=solve(prices,index+1,n,false,dp);
-             profit=Math.max(sell,nosell);
+            int sell=solve(nums,idx+2,0)+nums[idx];
+            int notsell=solve(nums,idx+1,1);
+            maxProfit=Math.max(sell,notsell);
         }
-        return dp[index][buy?1:0]=profit;
+
+        return dp[idx][state]=maxProfit;
     }
 }
