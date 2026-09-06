b2c487ea212ccc09b777bc28d816d3bf7881d707
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-09-06 09:17:01 +0530

    Time: 125 ms (5.09%) | Memory: 48.4 MB (8.21%) - LeetSync

--- Leetcode/45-jump-game-ii/jump-game-ii.java
+++ Leetcode/45-jump-game-ii/jump-game-ii.java
@@ -1,16 +1,21 @@
 class Solution {
+    Integer dp[];
     public int jump(int[] nums) {
-        int n=nums.length;
-        int currend=0;
-        int currfar=0;
-        int jump=0;
-        for(int i=0;i<n-1;i++){
-           currfar=Math.max(currfar,i+nums[i]);
-           if(i==currend){
-             jump++;
-             currend=currfar;
-           }
+        int n = nums.length;
+        this.dp=new Integer[n+1];
+        return solve(nums, n, 0);
+    }
+
+    public int solve(int nums[], int n, int start) {
+        if (start >= n - 1)
+            return 0;
+        if(dp[start]!=null) return dp[start];
+        int ele = nums[start];
+        int min = nums.length;
+
+        for (int i = 1; i <= ele && start + i < n; i++) {
+            min = Math.min(min, 1 + solve(nums, n, start + i));
         }
-        return jump;
+        return dp[start]=min;
     }
 }
