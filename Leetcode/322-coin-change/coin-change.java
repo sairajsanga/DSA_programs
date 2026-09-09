16c7f111b503cd330378d69e37b14b81466b5678
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-09-10 00:37:05 +0530

    Time: 51 ms (5.12%) | Memory: 47.5 MB (10.19%) - LeetSync

--- Leetcode/322-coin-change/coin-change.java
+++ Leetcode/322-coin-change/coin-change.java
@@ -4,12 +4,12 @@ class Solution {
        int n=coins.length;
        this.dp=new int[sum+1][n+1];
        for(int arr[]:dp) Arrays.fill(arr,-1);
-       return helper(coins,sum,0)==Integer.MAX_VALUE-1?-1:helper(coins,sum,0);
+       return helper(coins,sum,0)==Integer.MAX_VALUE/2?-1:helper(coins,sum,0);
     }
 
     public int helper(int nums[],int sum,int start){
 
-        if(start>=nums.length||sum<0) return Integer.MAX_VALUE-1;
+        if(start>=nums.length||sum<0) return Integer.MAX_VALUE/2;
         
         if(sum==0) return 0;
 
