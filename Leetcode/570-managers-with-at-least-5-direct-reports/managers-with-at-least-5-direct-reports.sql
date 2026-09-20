7902638fc382f72e8f8a91cac5210b4ed7e0a793
sairaj <154606239+sairajsanga@users.noreply.github.com>
2026-09-21 02:02:28 +0530

    Time: 521 ms (10.91%) | Memory: 0B (100.00%) - LeetSync

---
+++ Leetcode/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,32 @@
+# Write your MySQL query statement below
+
+
+select e.name from Employee e 
+    join Employee m on e.id=m.managerId
+    group by m.managerId having count(*)>=5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
